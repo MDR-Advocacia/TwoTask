229ad35f5d00c1ff63ea306234102c988a89bfe4
TI MDR <ti@mdradvocacia.com>
2026-05-05 14:46:21 -0300

    fix(models): registrar PrazoInicialPatrocinio e MasterVinculada no __init__ pra resolver relationship lazy

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -27,6 +27,8 @@ from .prazo_inicial_legacy_task_queue import PrazoInicialLegacyTaskCancellationI
 from .prazo_inicial_task_template import PrazoInicialTaskTemplate
 from .prazo_inicial_tipo_pedido import PrazoInicialTipoPedido
 from .prazo_inicial_pedido import PrazoInicialPedido
+from .prazo_inicial_patrocinio import PrazoInicialPatrocinio
+from .master_vinculada import MasterVinculada
 from .classification_taxonomy import (
     ClassificationCategory,
     ClassificationSubcategory,
